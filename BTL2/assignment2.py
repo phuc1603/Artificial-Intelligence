71ac5d30808d8225661583ca26c38f2356f76196
= <phuc1603>
2020-11-30 10:04:43 +0700

    update

--- BTL2/assignment2.py
+++ BTL2/assignment2.py
@@ -15,14 +15,27 @@ def initState(orders, nStaffs):
 
 def constFunc(staffs, orders, warehousePos):
     heuristic_result = 0
-
+        
+    listLoiNhuan = []
     for i in range(len(staffs)):
         posPre = warehousePos
         if not len(staffs[i]):
             return 99999999999
+
+        doanhThu = 0
+        chiPhi = 0
         for key in staffs[i]:
-            heuristic_result += abs(int(posPre[0])-int(staffs[i][key]['pos'][0])) + abs(int(posPre[1])-int(staffs[i][key]['pos'][1]))
+            doanhThu += 5 + int(staffs[i][key]['size']) + int(staffs[i][key]['weight'])*2
+
+            chiPhi += math.sqrt((int(posPre[0])-int(staffs[i][key]['pos'][0]))**2 + (int(posPre[1])-int(staffs[i][key]['pos'][1]))**2)
             posPre = staffs[i][key]['pos']
+        chiPhi = chiPhi*20/40 + 10
+        listLoiNhuan.append(doanhThu - chiPhi)
+
+    for i in range(len(listLoiNhuan)-1):
+        for j in range(i+1, len(listLoiNhuan)):
+            heuristic_result += abs(listLoiNhuan[i] - listLoiNhuan[j])
+        
     return heuristic_result
 
 def simulatedAnnealing(orders, nStaffs, warehousePos):
